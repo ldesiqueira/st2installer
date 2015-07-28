d46ce11df041df21a1f4fbf65d862bd619d13f98
Edward Medvedev <ed@doesnotcompute.ru>
2015-07-27 19:50:03 -0700

    SSL validation; cleanup

--- st2installer/controllers/keypair.py
+++ st2installer/controllers/keypair.py
@@ -7,8 +7,9 @@ class KeypairController(object):
 
   private = '/tmp/testkey'
   public = '/tmp/testkey-pub'
-  diff = '/etc/st2installer/keycompare'
   diff_output = '/tmp/keycompare.log'
+  ssh_diff = '/etc/st2installer/keycompare'
+  ssl_diff = '/etc/st2installer/sslcompare'
 
   @expose(generic=True, content_type='text/plain')
   def index(self):
@@ -19,16 +20,18 @@ class KeypairController(object):
     if request.POST['comparison'] == 'ssh':
       private_field = 'file-ssh-privatekey'
       public_field = 'file-ssh-publickey'
+      diff = self.ssh_diff
     else:
       private_field = 'file-privatekey'
       public_field = 'file-publickey'
+      diff = self.ssl_diff
     upload_private = request.POST[private_field]
     upload_public = request.POST[public_field]
     with open(self.private, 'w') as temp_private:
       temp_private.write(upload_private.file.read())
     with open(self.public, 'w') as temp_public:
       temp_public.write(upload_public.file.read())
-    call("%s > %s" % (self.diff, self.diff_output), shell=True)
+    call("%s > %s" % (diff, self.diff_output), shell=True)
     with open(self.diff_output, 'r') as output:
       data = output.read()
     return data
