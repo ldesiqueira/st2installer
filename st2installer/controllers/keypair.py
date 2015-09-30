5c55b408aaa3c9b930267053910d4d437d4bf75b
Tomaz Muraus <tomaz@tomaz.me>
2015-09-30 21:44:53 +0200

    Allow user to skip key check by passing ?skip_key_check=1 query param.

--- st2installer/controllers/keypair.py
+++ st2installer/controllers/keypair.py
@@ -1,11 +1,14 @@
 import tempfile
 import subprocess
+import six
 from six.moves import shlex_quote
+from six.moves.urllib import parse as urlparse
 from pecan import expose, request, response, redirect, abort
 import random, string, os
 from Crypto.PublicKey import RSA
 
 DEFAULT_RSA_KEY_SIZE = 2048
+
 
 class KeypairController(object):
 
@@ -45,7 +48,14 @@ class KeypairController(object):
       diff = self.ssl_diff
     upload_private = request.POST[private_field]
     upload_public = request.POST[public_field]
-    return self.compare(diff, upload_private.file.read(), upload_public.file.read())
+
+    skip_key_check = self._get_query_param_value(request=request, param_name='skip_key_check',
+                                                 param_type='bool', default_value=False)
+
+    if skip_key_check:
+        return 0
+    else:
+        return self.compare(diff, upload_private.file.read(), upload_public.file.read())
 
   @expose('json')
   def keygen(self):
@@ -63,3 +73,39 @@ class KeypairController(object):
   def public(self):
     response.headers['Content-Disposition'] = 'attachment; filename="st2-ssh.pub"'
     return self.gen_public.exportKey('OpenSSH')
+
+  def _parse_query_params(self, request):
+    """
+    Parse query string for the provided request.
+
+    :rtype: ``dict``
+    """
+    query_string = request.query_string
+    query_params = dict(urlparse.parse_qsl(query_string))
+
+    return query_params
+
+  def _get_query_param_value(self, request, param_name, param_type, default_value=None):
+    """
+    Return a value for the provided query param and optionally cast it for boolean types.
+
+    If the requested query parameter is not provided, default value is returned instead.
+
+    :param request: Request object.
+
+    :param param_name: Name of the param to retrieve the value for.
+    :type param_name: ``str``
+
+    :param param_type: Type of the query param (e.g. "bool").
+    :type param_type: ``str``
+
+    :param default_value: Value to return if query param is not provided.
+    :type default_value: ``object``
+    """
+    query_params = self._parse_query_params(request=request)
+    value = query_params.get(param_name, default_value)
+
+    if param_type == 'bool' and isinstance(value, six.string_types):
+      value = value.lower() in ['1', 'true']
+
+    return value
